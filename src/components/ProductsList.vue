67a31787c7edbf53a808109c070e053f657e620e
Carlos Gonzalez <siscond@hotmail.com>
2022-07-31 12:36:51 -0400

    Initial Commit

---
+++ src/components/ProductsList.vue
@@ -0,0 +1,24 @@
+<template>
+    <div v-if="cartItems.length > 0">
+        <ProductsListItem 
+            v-for="product in cartItems"
+            :key="product.id"
+            :product="product" />
+    </div>
+    <p v-else>You haven't added products to your cart yet!</p>
+</template>
+
+<script>
+import ProductsListItem from './ProductsListItem.vue';
+export default {
+    name: 'ProductsList',
+    props: ['cartItems'],
+    components: {
+        ProductsListItem,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
